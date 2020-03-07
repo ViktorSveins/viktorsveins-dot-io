b5628bc661fc9f4300222b697234806ee172246b
ViktorSveins <viktorsveins@gmail.com>
2020-03-07 20:06:28 +0000

    Polygon svg background

---
+++ src/components/Polygons/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './polygons-style.scss';
+
+const Polygons = () => {
+    return(
+        <div class="polygons">
+            <svg class="up-left side" />
+            <svg class="up-mid"/>
+            <svg class="up-right side"/>
+        </div>
+    )
+}
+
+export default Polygons;
